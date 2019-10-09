5098d43b6eb409a128bf375729de4573d5958ac2
Diogo Alexandria <diogoagrasmsdc@gmail.com>
2019-10-09 19:22:54 -0300

    Criação da arena

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function setup() {
-    
+    // Canvas com fundo preto    
+    createCanvas(560, 560);
+    background(0,0,0);      
 }
   
 function draw() {
-    ellipse(50, 50, 80, 80);
+    // Arena com fundo preto e linhas branca
+    fill(0,0,0);
+    stroke(255,255,255);
+    rect(30, 30, 500, 500);
+
+    // Entrada e saída
+    rect(10, 30, 20, 20);    
+    rect(530, 510, 20, 20);
 }
