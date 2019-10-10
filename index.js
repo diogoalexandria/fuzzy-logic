6a985ddc6de887c20363e7dc08c5303e1a136dc3
Diogo Alexandria <diogoagrasmsdc@gmail.com>
2019-10-09 21:16:13 -0300

    Robô no caminho de casa

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
+var positionX = 15;
+var positionY = 35;
+
 function setup() {
-    // Canvas com fundo preto    
+    // Cria canvas   
     createCanvas(560, 560);
-    background(0,0,0);      
+    ;      
 }
   
 function draw() {
+    //
+    background(0, 0, 0);
     // Arena com fundo preto e linhas branca
-    fill(0,0,0);
-    stroke(255,255,255);
+    fill(0, 0, 0);
+    stroke(255, 255, 255);
     rect(30, 30, 500, 500);
 
-    // Entrada e saída
-    rect(10, 30, 20, 20);    
+    // Entrada
+    rect(10, 30, 20, 20);
+    // Saída    
     rect(530, 510, 20, 20);
+
+    // Robô
+    fill(100)
+    if (positionX < 30) {
+        xAxisMoviment();
+    }
+    else if (positionX + 10 < 520) {
+        xAxisMoviment();
+        yAxisMoviment();
+    }
+    else if (positionX + 10 < 545 ) {
+        xAxisMoviment();
+    }
+    // xAxisMoviment();
+    // yAxisMoviment();
+    rect(positionX, positionY, 10, 10);
+}
+
+function xAxisMoviment() {
+    positionX = positionX + 1;
+}
+
+function yAxisMoviment() {
+    positionY = positionY + 1;
 }
