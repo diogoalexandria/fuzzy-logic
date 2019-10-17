84200de34204a5771ed6c1efb9dbda2a3c30c9dc
Diogo Agra Alexandria <diogoaa@ipiranga.ipiranga>
2019-10-17 16:54:36 -0300

    Adição de lógica ao movimento do robô

--- index.js
+++ index.js
@@ -1,50 +1,160 @@
-var positionX = 15;
-var positionY = 35;
+var columns = 50;
+var rows = 50;
+var grid = new Array(columns);
+
+var widthArena;
+var heightArena;
+var start;
+var end;
+var currentPosition;
+var lastMoviment;
+var path = [];
+
+function Spot(i, j) {
+    this.x = i;
+    this.y = j;
+    this.neighbors = [];
+    this.wall = false;
+    this.visited = false;
+
+    if (random(1) < 0.1) {
+        this.wall = true;
+    }
+
+    this.show = function (color) {
+        fill(color);
+        if (this.wall) {
+            fill(0);
+        }
+        // noStroke();           
+        rect(this.x * widthSpotArena, this.y * heightSpotArena, widthSpotArena, heightSpotArena);
+    }
+
+    this.addNeighbors = function (grid) {
+        if (this.x < columns - 1) {
+            this.neighbors.push(grid[this.x + 1][this.y]);
+        }
+        if (this.x > 0) {
+            this.neighbors.push(grid[this.x - 1][this.y]);
+        }
+        if (this.y < rows - 1) {
+            this.neighbors.push(grid[this.x][this.y + 1]);
+        }
+        if (this.y > 0) {
+            this.neighbors.push(grid[this.x][this.y - 1]);
+        }
+        if (this.x > 0 && this.y > 0) {
+            this.neighbors.push(grid[this.x - 1][this.y - 1])
+        }
+        if (this.x > columns - 1 && this.y > 0) {
+            this.neighbors.push(grid[this.x + 1][this.y - 1])
+        }
+        if (this.x > 0 && this.y > rows - 1) {
+            this.neighbors.push(grid[this.x - 1][this.y + 1])
+        }
+        if (this.x > columns - 1 && this.y > rows - 1) {
+            this.neighbors.push(grid[this.x + 1][this.y + 1])
+        }
+    }
+}
 
 function setup() {
-    // Cria canvas   
-    createCanvas(560, 560);
-    CreateBoard();
-}
-  
-function draw() {    
-    background(0, 0, 0);
-    // Arena com fundo preto e linhas branca
-    fill(0, 0, 0);
-    stroke(255, 255, 255);
-    rect(30, 30, 500, 500);
+    createCanvas(500, 500);
 
-    // Entrada
-    rect(10, 30, 20, 20);
-    // Saída    
-    rect(530, 510, 20, 20);
+    widthSpotArena = width / columns;
+    heightSpotArena = height / rows;
 
-    
-    if (positionX < 30) {
-        // Se tá dentro da área de entrada anda para direita
-        xAxisMoviment();
+    // Criando a matriz
+    for (var i = 0; i < columns; i++) {
+        grid[i] = new Array(rows);
+    };
+
+    for (var i = 0; i < columns; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Spot(i, j);
+        }
     }
-    else if (positionX + 10 < 520) {
-        // Anda em diagonal
-        xAxisMoviment();
-        yAxisMoviment();
+
+    for (var i = 0; i < columns; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].addNeighbors(grid);
+        }
     }
-    else if (positionX + 10 < 545 ) {
-        // Se chegou a 5 pixels do final anda só a direita
-        xAxisMoviment();
+
+    start = grid[0][0];
+    currentPosition = start;
+    end = grid[columns - 1][rows - 1];
+    start.wall = false;
+    end.wall = false;
+}
+
+function draw() {
+    background(0)
+
+    if (currentPosition === end) {
+        alert("Encontrei a saída!");
+        noLoop();
     }
-    // xAxisMoviment();
-    // yAxisMoviment();
 
-    // Robô
-    fill(100)
-    rect(positionX, positionY, 10, 10);
+    var downPosition = grid[currentPosition.x][currentPosition.y + 1];
+    var rightPosition = grid[currentPosition.x + 1][currentPosition.y];
+    var upPosition = grid[currentPosition.x][currentPosition.y - 1];
+    // var leftPosition = grid[currentPosition.x - 1][currentPosition.y];
+
+    var availableDownPosition = downPosition ? downPosition.visited === false && downPosition.wall === false : false;
+    var availableRightPosition = rightPosition.visited === false && rightPosition.wall === false;
+    var availableUpPosition = upPosition ? upPosition.visited === false && upPosition.wall === false : false;
+    // var availableLeftPosition = leftPosition? 
+
+    if (availableDownPosition && lastMoviment != 1) {
+        path.push(currentPosition);
+        currentPosition = downPosition;
+        lastMoviment = 1;
+    } else if (availableRightPosition) {
+        path.push(currentPosition);
+        currentPosition = rightPosition;
+        lastMoviment = 2;
+    } else if (availableDownPosition) {
+        path.push(currentPosition);
+        currentPosition = downPosition;
+        lastMoviment = 1;
+    } else if (availableUpPosition) {
+        path.push(currentPosition);
+        currentPosition = upPosition;
+        lastMoviment = 1;
+    }
+
+    for (var i = 0; i < columns; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].show(color(255));
+        }
+    }
+
+    for (var i = 0; i < path.length; i++) {
+        path[i].show(color(255, 0, 0));
+    }
+
+    // for (var i = 0; i < openSet.length; i++) {
+    //     currentPosition.show(color(0,255,0))
+    // }
+
+    currentPosition.show(color(0, 255, 0));
+
+
+    // for (var i = 0; i < path.length; i++) {
+    //     path[i].show(color(0, 0, 255));
+    // }
 }
 
-function xAxisMoviment() {
-    positionX = positionX + 1;
-}
+// var moviment= function(currentPosition, locationMoviment, pathTavaled) {
+//     pathTraveled.push(currentPosition);
+//     currentPosition = downPosition;
+// }
 
-function yAxisMoviment() {
-    positionY = positionY + 1;
-}
+// function xAxisMoviment() {
+//     positionX = positionX + 1;
+// }
+
+// function yAxisMoviment() {
+//     positionY = positionY + 1;
+// }
